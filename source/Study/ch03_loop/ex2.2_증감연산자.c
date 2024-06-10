9f2784fd5d41e1c741dd7f82ce3c91c29721eb86
puble123 <128446819+puble123@users.noreply.github.com>
2024-06-10 23:16:51 +0900

    Delete source/Study directory

--- source/Study/ch03_loop/ex2.2_증감연산자.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// 증감연산자 : ++ --
-int main(void){
-    int n1 = 10;
-    printf("++n1 = %d\n", ++n1);
-    printf("n1++ = %d\n", n1++);
-    printf("n1=%d\n", n1);
-}
--- source/Study/ch03_loop/ex3.2_while.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void){
-    int i=1;
-    while(i<=10){
-        printf("%d. Hello, World\n", i++);
-    }
-}
